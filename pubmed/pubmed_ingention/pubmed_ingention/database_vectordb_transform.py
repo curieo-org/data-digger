6ce1656a781fa05aafa2df9caa6872db14608e82
raahulmona <157174139+raahulmona@users.noreply.github.com>
2024-03-22 23:21:58 +0100

    Adding sparse vector capability

--- pubmed/pubmed_ingention/pubmed_ingention/database_vectordb_transform.py
+++ pubmed/pubmed_ingention/pubmed_ingention/database_vectordb_transform.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 from functools import partial
@@ -15,6 +15,17 @@ from llama_index.embeddings.text_embeddings_inference import TextEmbeddingsInfer
 from sqlalchemy import create_engine, text
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
+from typing import Any, List, Tuple
+import torch
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+doc_splade_model = "naver/efficient-splade-VI-BT-large-doc"
+query_splade_model = "naver/efficient-splade-VI-BT-large-query"
+doc_tokenizer = AutoTokenizer.from_pretrained(doc_splade_model)
+doc_model = AutoModelForMaskedLM.from_pretrained(doc_splade_model)
+
+query_tokenizer = AutoTokenizer.from_pretrained(query_splade_model)
+query_model = AutoModelForMaskedLM.from_pretrained(query_splade_model)
 
 
 class PubmedDatabaseReader(DatabaseReader):
@@ -61,22 +72,106 @@ class PubmedDatabaseReader(DatabaseReader):
 
         
 class DatabaseVectorsEngine:
+
+    def sparse_doc_vectors(
+            self,
+            texts: List[str],
+        ) -> Tuple[List[List[int]], List[List[float]]]:
+        """
+        Computes vectors from logits and attention mask using ReLU, log, and max operations.
+        """
+        tokens = doc_tokenizer(
+            texts, truncation=True, padding=True, return_tensors="pt"
+        )
+        if torch.cuda.is_available():
+            tokens = tokens.to("cuda")
+
+        output = doc_model(**tokens)
+        logits, attention_mask = output.logits, tokens.attention_mask
+        relu_log = torch.log(1 + torch.relu(logits))
+        weighted_log = relu_log * attention_mask.unsqueeze(-1)
+        tvecs, _ = torch.max(weighted_log, dim=1)
+
+        # extract the vectors that are non-zero and their indices
+        indices = []
+        vecs = []
+        for batch in tvecs:
+            indices.append(batch.nonzero(as_tuple=True)[0].tolist())
+            vecs.append(batch[indices[-1]].tolist())
+
+        return indices, vecs
+    
+    def sparse_query_vectors(
+            self,
+            texts: List[str],
+        ) -> Tuple[List[List[int]], List[List[float]]]:
+        """
+        Computes vectors from logits and attention mask using ReLU, log, and max operations.
+        """
+        # TODO: compute sparse vectors in batches if max length is exceeded
+        tokens = query_tokenizer(
+            texts, truncation=True, padding=True, return_tensors="pt"
+        )
+        if torch.cuda.is_available():
+            tokens = tokens.to("cuda")
+
+        output = query_model(**tokens)
+        logits, attention_mask = output.logits, tokens.attention_mask
+        relu_log = torch.log(1 + torch.relu(logits))
+        weighted_log = relu_log * attention_mask.unsqueeze(-1)
+        tvecs, _ = torch.max(weighted_log, dim=1)
+
+        # extract the vectors that are non-zero and their indices
+        indices = []
+        vecs = []
+        for batch in tvecs:
+            indices.append(batch.nonzero(as_tuple=True)[0].tolist())
+            vecs.append(batch[indices[-1]].tolist())
+
+        return indices, vecs
+
     def __init__(
             self,
             qdrant_url_address:str,
             qdrant_url_port:int,
-            qdrant_collection_name:str
+            qdrant_collection_name:str,
+            qdrant_api_key:str
             ):
-        self.client = QdrantClient(qdrant_url_address, port=qdrant_url_port)
-        self.client.create_collection(
-            collection_name=qdrant_collection_name,
-            vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE),
+        self.client = QdrantClient(
+            url=qdrant_url_address,
+            port=qdrant_url_port, 
+            api_key=qdrant_api_key,
+            https=False
             )
-        self.vector_store = QdrantVectorStore(client=self.client, collection_name=qdrant_collection_name)
+        
+        self.client.recreate_collection(
+            collection_name=qdrant_collection_name,
+            vectors_config={
+                "text-dense": models.VectorParams(
+                    size=1024,
+                    distance=models.Distance.COSINE,
+                )
+            },
+            sparse_vectors_config={
+                "text-sparse": models.SparseVectorParams(
+                    index=models.SparseIndexParams(
+                        on_disk=False,
+                        )
+                    )
+                }
+            )
+        
+        self.vector_store = QdrantVectorStore(
+            client=self.client,
+            collection_name=qdrant_collection_name,
+            enable_hybrid=True,
+            sparse_doc_fn=self.sparse_doc_vectors,
+            sparse_query_fn=self.sparse_query_vectors,
+            )
 
         self.node_parser = SimpleNodeParser.from_defaults(chunk_size=1024, chunk_overlap=32)
         self.service_context = ServiceContext.from_defaults(
-            embed_model=TextEmbeddingsInference(model_name="model_name_here", timeout=60, embed_batch_size=4),
+            embed_model=TextEmbeddingsInference(model_name="", timeout=60, embed_batch_size=1),
             llm=None,
             node_parser=self.node_parser
         )
@@ -93,7 +188,7 @@ class DatabaseVectorsEngine:
 
         self.batch_size = 100
         self.chunk_size = 100
-        
+
 
     @staticmethod
     def process_records_chunk(chunk, service_context, storage_context):
