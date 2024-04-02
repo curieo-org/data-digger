f4a529f1480c180f854f1fb2d1d92aeeb77d8378
Rathijit Paul <alienpapon009@gmail.com>
2024-04-02 18:22:13 +0600

    :construction: add vectorization process for clinical trial data

--- clinical_trail/app/config.py
+++ clinical_trail/app/config.py
@@ -2,3 +2,8 @@ PG_LOCAL_DATABASE = 'postgresql://postgres@localhost:5430/aact'
 PG_PROD_DATABASE = 'postgresql://postgres@localhost:5431/aact'
 
 PG_PROD_SETUP_FILE = 'assets/prod_setup.sql'
+
+QDRANT_HOST = 'localhost'
+QDRANT_PORT = 6333
+QDRANT_COLLECTION_NAME = 'clinical_trials_vector_db'
+QDRANT_API_KEY = 'z0U3Kdzu'
--- clinical_trail/app/main.py
+++ clinical_trail/app/main.py
@@ -1,6 +1,8 @@
 import asyncio
 from app.database_transfer import transfer_all_tables
 from app.scraper import scrape_clinical_trial_database, setup_local_database, remove_local_database
+from app.vector_transfer import ClinicalTrailVectorDbEngine
+from app.config import PG_LOCAL_DATABASE, QDRANT_HOST, QDRANT_PORT, QDRANT_COLLECTION_NAME, QDRANT_API_KEY
 
 async def async_main():
     print('Starting the scraping process and setting up the local database')
@@ -9,6 +11,15 @@ async def async_main():
 
     print('Starting the data transfer process')
     await transfer_all_tables()
+
+    print('Starting the vector generation and transfer process')
+    ctve = ClinicalTrailVectorDbEngine(
+        qdrant_url_address=QDRANT_HOST,
+        qdrant_url_port=QDRANT_PORT,
+        qdrant_collection_name=QDRANT_COLLECTION_NAME,
+        qdrant_api_key=QDRANT_API_KEY
+    )
+    ctve.database_to_vectors(database_engine=PG_LOCAL_DATABASE)
 
     print('Removing the local database')
     await remove_local_database()
