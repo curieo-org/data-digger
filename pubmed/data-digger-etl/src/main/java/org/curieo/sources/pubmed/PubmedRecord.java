d171ac31b3987505ea76b9c9436cfc66c420051a
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-04-30 08:48:25 +0200

    revert MedlineCitation extraction

--- pubmed/data-digger-etl/src/main/java/org/curieo/sources/pubmed/PubmedRecord.java
+++ pubmed/data-digger-etl/src/main/java/org/curieo/sources/pubmed/PubmedRecord.java
@@ -248,9 +248,6 @@ public class PubmedRecord implements Record {
           case "Citation":
             citation = readText(reader, "Citation");
             break;
-          case "MedlineCitation":
-            citation = readText(reader, "MedlineCitation");
-            break;
           case PubmedRecord.ARTICLEID_TAG:
             identifiers.add(PubmedRecord.readArticleId(reader, startElement));
             break;
