813899d80a9ed362eb2276e979d11f66d0e9735e
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-05-01 00:20:15 +0200

    Update fill-citation-counts.sql based on new reference table definition alt 2

--- pubmed/data-digger-etl/scripts/sql/fill-citation-counts.sql
+++ pubmed/data-digger-etl/scripts/sql/fill-citation-counts.sql
@@ -1,6 +1,5 @@
 
-SELECT reference AS PubmedId, COUNT(reference) AS citationcount
+SELECT reference, reference_type, COUNT(reference) AS citationcount
 INTO citationcountswithoutyear
 FROM referencetable
-where reference_type == 0
-GROUP BY reference
+GROUP BY reference, reference_type
