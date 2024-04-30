286485e25f8cbbb78f17909a4e9006ce24dc85fe
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-04-30 08:52:41 +0200

    Remove unused Objects import

--- pubmed/data-digger-etl/src/main/java/org/curieo/consumer/SQLSinkFactory.java
+++ pubmed/data-digger-etl/src/main/java/org/curieo/consumer/SQLSinkFactory.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.Generated;
 import org.curieo.model.Authorship;
