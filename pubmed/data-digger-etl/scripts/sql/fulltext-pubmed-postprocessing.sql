331b2711740ec0039726a00aa76e9e0f77beb5a2
Rathijit Paul <30369246+rathijitpapon@users.noreply.github.com>
2024-05-02 17:27:33 +0600

    :construction: added ranking job and updated baseline dockerfile

--- pubmed/data-digger-etl/scripts/sql/fulltext-pubmed-postprocessing.sql
+++ pubmed/data-digger-etl/scripts/sql/fulltext-pubmed-postprocessing.sql
@@ -1,5 +1,5 @@
 -- Add Location column to linktable
-alter table linktable add location text;
+alter table linktable add column if not exists location text;
 
 -- Update location column in linktable with the s3 location
 update linktable lt set location = ftd.location from fulltextdownloads ftd where ftd.identifier = lt.pmc;
