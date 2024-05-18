10f53613a08340552955ab989c11516a2699d19b
Rathijit Paul <30369246+rathijitpapon@users.noreply.github.com>
2024-05-18 15:27:53 +0600

    :bug: fix bash and psql credentials bug

--- pubmed/fulltext-pmc-xml/update-fulltext-xmls.sh
+++ pubmed/fulltext-pmc-xml/update-fulltext-xmls.sh
@@ -20,10 +20,12 @@ Completed=2
 Failed=3
 Unavailable=4
 
+export PGPASSWORD=$PASSWORD
+
 process_ftp_files() {
     ftp_files="$1"
     job_name="$2"
-    files_to_be_updates=()
+    files_to_be_updates=$()
     
     for file in $ftp_files; do
         file_name=$(echo $file | awk -F'/' '{print $NF}')
@@ -36,7 +38,7 @@ process_ftp_files() {
             query="INSERT INTO pmctasks (name, state, job, timestamp) VALUES ('$file_name', $InProgress, '$job_name', now()) ON CONFLICT (name, job) DO UPDATE SET state = $InProgress, timestamp = now();"
             psql -h $HOST -p $PORT -U $USERNAME -d $DATABASE -c "$query"
 
-            files_to_be_updates+=("$file_name")
+            files_to_be_updates+=$("$file_name")
 
             wget --ftp-user='anonymous' --ftp-password='anonymous' --continue $file
         fi
